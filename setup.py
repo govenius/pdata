d89ff1ee98bfb97e84d2113fc4e3ce057234784f
Joonas Govenius <joonas.govenius@gmail.com>
2022-03-27 21:50:05 +0300

    Bump version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pdata',
-      version='1.0.1',
+      version='1.0.2',
       author='Joonas Govenius',
       license='MIT',
       license_files=('LICENSE',),
