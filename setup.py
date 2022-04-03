354dc7433879d1ae331c2aee2685d0ea2a1d8554
Joonas Govenius <joonas.govenius@gmail.com>
2022-04-03 21:17:00 +0300

    Migrate to setup.cfg

--- setup.py
+++ setup.py
@@ -1,9 +1,2 @@
-from setuptools import setup, find_packages
-
-setup(name='pdata',
-      version='1.0.3',
-      author='Joonas Govenius',
-      license='MIT',
-      license_files=('LICENSE',),
-      packages=['pdata', 'pdata.analysis']
-      )
+from setuptools import setup
+setup()
